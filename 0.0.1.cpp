9d29ede0a7b55812e974dca13f0f08f93a8d3c77
DanilBukreev <31454061+DanilBukreev@users.noreply.github.com>
2017-11-13 14:14:29 +0300

    Update 0.0.1.cpp

--- 0.0.1.cpp
+++ 0.0.1.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  int n, m;
-  cout << "Введите размер массива" << endl;
-  cin >> n;
-  if (n <= 0) {
+int main(){
+ int n, m;
+ cout << "Введите размер массива" << endl;
+ cin >> n;
+  if ( n <= 0 ) {
     cout << "error" << endl;
-  }
-  int a[n];
-  for (m = 0; m < n; m++) {
+    return 1;
+ }
+ cout << "Введите элементы массива" << endl;
+ int *a = new int [n];
+ for (m=0; m<n; m++){
     cin >> a[m];
-  }
-  for (m = 0; m < n / 2; m++) {
-    swap(a[m], a[n - m - 1]);
-  }
-  for (m = 0; m < n; m++) {
-    if (n != m) {
-      cout << "error" << endl;
-      return 0;
-    }
+ }
+ for (m=0; m<n/2; m++){
+    swap (a[m], a[n-m-1]);
+ }
+ for (m=0; m<n; m++){
     cout << a[m] << endl;
-  }
+ }
+ delete [] a;
 }
 
